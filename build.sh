bb790276a7ef4dfa735f4ea212637fddac7685be
Ragunath Jawahar <ragunath@redgreen.io>
2021-08-29 15:25:48 +0530

    chore: add script to build Linux and macOS binaries

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+set -e
+echo "Building Linux binary"
+env GOOS=linux GOARCH=arm go build -v -o bin/linux/watchtower
+
+echo "Building macOS binary"
+env GOOS=darwin GOARCH=amd64 go build -v -o bin/macOS/watchtower
--- watchtower.go
+++ watchtower.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "log"
+  "net/http"
+)
+import _ "net/http"
 
 func main() {
-  fmt.Println("hello world")
+  resp, err := http.Get("https://google.com")
+  if err != nil {
+    log.Fatalln("Uh oh! unable to fetch URL.")
+  }
+  fmt.Println(resp.Status)
 }
