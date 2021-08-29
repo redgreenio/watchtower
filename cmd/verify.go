4571ee2ffc0c945f7f47c30b9cb749d2bdc80e02
Ragunath Jawahar <ragunath@redgreen.io>
2021-08-29 17:38:09 +0530

    feat: enable `verify` subcommand to use any app ID

--- cmd/verify.go
+++ cmd/verify.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+  "errors"
   "fmt"
   "github.com/spf13/cobra"
   "watchtower/download"
@@ -9,8 +10,15 @@ import (
 
 var verifyCmd = &cobra.Command{
   Use: "verify",
+  Args: func(cmd *cobra.Command, args []string) error {
+    if len(args) < 1 {
+      return errors.New("missing app ID")
+    }
+    return nil
+  },
   Run: func(cmd *cobra.Command, args []string) {
-    content := download.Download("com.spotify.music")
+    appId := args[0]
+    content := download.Download(appId)
     appListing := parser.Parse(content)
     fmt.Println(appListing.Name)
   },
