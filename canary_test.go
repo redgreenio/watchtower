672c219fe509fac3d5c768d51b6b0fdfe7c2a774
Ragunath Jawahar <ragunath@redgreen.io>
2021-08-29 15:12:12 +0530

    chore: setup a new go project ✨

---
+++ canary_test.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+  "github.com/stretchr/testify/assert"
+  _ "github.com/stretchr/testify/assert"
+  "testing"
+  _ "testing"
+)
+
+func TestCanary(t *testing.T) {
+  assert.True(t, true)
+}
