af1b5b92ab86c4c9eccfd22d284ae827f4adece5
Ragunath Jawahar <ragunath@redgreen.io>
2021-08-30 18:22:12 +0530

    chore: rename `watchtower` binary to `xwatchtower` (experimental)

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -e
 echo "Building Linux binary"
-env GOOS=linux GOARCH=arm go build -v -o bin/linux/watchtower
+env GOOS=linux GOARCH=arm go build -v -o bin/linux/xwatchtower
 
 echo "Building macOS binary"
-env GOOS=darwin GOARCH=amd64 go build -v -o bin/macOS/watchtower
+env GOOS=darwin GOARCH=amd64 go build -v -o bin/macOS/xwatchtower
