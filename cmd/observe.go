5931d3103feccc87d76b08d8e3daaf3d2eaa197d
Ragunath Jawahar <ragunath@redgreen.io>
2021-09-11 08:02:58 +0530

    feat: download listings in parallel

--- cmd/observe.go
+++ cmd/observe.go
@@ -4,6 +4,8 @@ import (
   "fmt"
   "github.com/fatih/color"
   "github.com/spf13/cobra"
+  "sync"
+  "time"
   "watchtower/app"
   "watchtower/database"
   "watchtower/download"
@@ -11,31 +13,64 @@ import (
   "watchtower/release"
 )
 
+var (
+    counter int64 = 1
+    mutex   sync.Mutex
+    waitGroup sync.WaitGroup
+    dbMutex sync.Mutex
+)
+
 var red = color.New(color.FgRed)
 
 var observeCmd = &cobra.Command{
   Use: "observe",
   Run: func(cmd *cobra.Command, args []string) {
+    start := time.Now()
     database := database.InitDb(database.DbPath)
     appsRepository := app.DefaultAppsRepository{Database: database}
     releasesRepository := release.DefaultReleasesRepository{Database: database}
 
     apps := appsRepository.List()
     appsCount := len(apps)
-    for index, app := range apps {
-      println(fmt.Sprintf("[%d/%d] Downloading release info for '%s'", index+1, appsCount, app.AppId))
-      content, err := download.Download(app.AppId, app.Country)
-      if err != nil {
-        println(fmt.Sprintf("[%d/%d] Download failed for '%s'", index+1, appsCount, app.AppId))
-        continue
-      }
-      release := parser.Parse(content)
-      if release.AppId == "" {
-        _, _ = red.Printf("Unable to parse HTML for '%s'", app.AppId)
-        println()
-        continue
-      }
-      releasesRepository.Insert(release)
+
+    waitGroup.Add(len(apps))
+
+    for _, app := range apps {
+      go downloadAndSaveReleaseInformation(app, appsCount, releasesRepository, &waitGroup)
     }
+
+    waitGroup.Wait()
+    elapsed := time.Since(start)
+    println(fmt.Sprintf("~ Observation took %dms ~", elapsed.Milliseconds()))
   },
+}
+
+func downloadAndSaveReleaseInformation(
+  app app.App,
+  appsCount int,
+  releasesRepository release.ReleasesRepository,
+  waitGroup *sync.WaitGroup,
+) {
+  var count int64
+  mutex.Lock()
+  count = counter
+  println(fmt.Sprintf("[%d/%d] Downloading release info for '%s'", count, appsCount, app.AppId))
+  counter++
+  mutex.Unlock()
+
+  content, err := download.Download(app.AppId, app.Country)
+  if err != nil {
+    println(fmt.Sprintf("[%d/%d] Download failed for '%s'", count, appsCount, app.AppId))
+  } else {
+    appRelease := parser.Parse(content)
+    if appRelease.AppId == "" {
+      _, _ = red.Printf("Unable to parse HTML for '%s'", app.AppId)
+      println()
+    } else {
+      dbMutex.Lock()
+      releasesRepository.Insert(appRelease)
+      dbMutex.Unlock()
+    }
+  }
+  defer waitGroup.Done()
 }
