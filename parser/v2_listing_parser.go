a4474adc4ca52c207fd8eb14fe0deb898d89d96d
Ragunath Jawahar <ragunath@redgreen.io>
2022-11-13 17:17:41 +0530

    feat: parse released on date

--- parser/v2_listing_parser.go
+++ parser/v2_listing_parser.go
@@ -3,7 +3,10 @@ package parser
 import (
   "github.com/PuerkitoBio/goquery"
   "strings"
+  "time"
 )
+
+const ReleasedOnDateLayoutV2 string = "2 Jan 2006"
 
 type V2ListingParser struct {
 }
@@ -12,10 +15,11 @@ func (parser V2ListingParser) Parse(content string) Release {
   document, _ := goquery.NewDocumentFromReader(strings.NewReader(content))
 
   return Release{
-    Name:      getNameV2(document),
-    AppId:     getAppIdV2(document),
-    Installs:  getInstallsV2(document),
-    OfferedBy: getOfferedByV2(document),
+    Name:       getNameV2(document),
+    AppId:      getAppIdV2(document),
+    ReleasedOn: getReleasedOnV2(document),
+    Installs:   getInstallsV2(document),
+    OfferedBy:  getOfferedByV2(document),
   }
 }
 
@@ -42,4 +46,13 @@ func getInstallsV2(document *goquery.Document) string {
 func getOfferedByV2(document *goquery.Document) string {
   offeredBySpan := document.Find("div > a > span").First()
   return strings.TrimSpace(offeredBySpan.Text())
+}
+
+func getReleasedOnV2(document *goquery.Document) time.Time {
+  releasedOnText := document.Find(".xg1aie").First().Text()
+  dateTextParts := strings.Split(releasedOnText, " ")
+  threeLetterMonth := dateTextParts[1][0:3]
+  sanitizedDateTextParts := [3]string{dateTextParts[0], threeLetterMonth, dateTextParts[2]}
+  releasedOnDate, _ := time.Parse(ReleasedOnDateLayoutV2, strings.Join(sanitizedDateTextParts[:], " "))
+  return releasedOnDate
 }
--- parser/v2_listing_parser_test.go
+++ parser/v2_listing_parser_test.go
@@ -3,16 +3,19 @@ package parser
 import (
   "github.com/stretchr/testify/assert"
   "testing"
+  "time"
 )
 
 func TestParseV2Listing(t *testing.T) {
   // given
   content := ReadTestDataFile("v2-listing.html")
+  releasedOn, _ := time.Parse(ReleasedOnDateLayoutV2, "29 Sep 2022")
   expected := Release{
-    Name:      "Hotstar",
-    AppId:     "in.startv.hotstar",
-    Installs:  "500M+",
-    OfferedBy: "Novi Digital",
+    Name:       "Hotstar",
+    AppId:      "in.startv.hotstar",
+    ReleasedOn: releasedOn,
+    Installs:   "500M+",
+    OfferedBy:  "Novi Digital",
   }
 
   // when
